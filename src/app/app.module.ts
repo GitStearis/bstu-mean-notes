736a004076f7591d95b33fbada110233c10fcf3a
GitStearis <egor.puisha@gmail.com>
2019-04-01 23:32:17 +0300

    Add Angular Material, update TSlint preferences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NewNoteComponent } from './components/new-note/new-note.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewNoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
